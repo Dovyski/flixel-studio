33ed93476584b5c3dd258bfebcd71cdeb08f0ec4
Fernando Bevilacqua <dovyski@gmail.com>
2018-08-15 09:12:10 +0200

    Initial implementation of stackable windows

--- flixel/addons/studio/ui/Window.hx
+++ flixel/addons/studio/ui/Window.hx
@@ -10,6 +10,9 @@ import flash.geom.Rectangle;
  */
 class Window extends flixel.system.debug.Window
 {
+	var _siblingLeft:Window;
+	var _siblingRight:Window;
+	
 	/**
 	 * Creates a new window object.  This Flash-based class is mainly (only?) used by FlxDebugger.
 	 * 
@@ -26,5 +29,97 @@ class Window extends flixel.system.debug.Window
 	{
 		super(title, icon, width, height, resizable, bounds, closable);
 		visible = true;
+	}
+
+	function updateBasedOnSibling(commander:Window, toTheRight:Bool = true, offsetX:Float = 0):Void
+	{
+		// Decide on the next sibling based on the informed flow,
+		// i.e. to the right or to the left.
+		var next = toTheRight ? _siblingRight : _siblingLeft;
+		
+		// offsetX is our position in relation to the window being dragged, i.e. commander
+		var nextOffsetX = offsetX + (toTheRight ? 1 : -1) * getTitleTabWidth();
+
+		if (commander != null && commander != this)
+		{
+			x = commander.x + offsetX + (toTheRight ? 0 : -getTitleTabWidth());
+			y = commander.y;
+		}
+
+		if (next != null)
+			next.updateBasedOnSibling(commander, toTheRight, nextOffsetX);
+	}
+
+	override public function reposition(x:Float, y:Float):Void
+	{
+		super.reposition(x, y);
+
+		if (_siblingRight != null)
+			_siblingRight.updateBasedOnSibling(this, true, getTitleTabWidth());
+		if (_siblingLeft != null)
+			_siblingLeft.updateBasedOnSibling(this, false);
+	}
+
+	public function getTitleTabWidth():Float
+	{
+		return _title.textWidth + 10;
+	}
+
+	public function hasSiblings():Bool
+	{
+		return _siblingLeft != null || _siblingRight != null;
+	}
+
+	public function attachTo(target:Window):Void
+	{
+		if (target == null)
+			throw "target window to be attached to must not be null";
+
+		var lastSibling = target.getLastRightSibling();
+
+		if (lastSibling == null)
+			// Target has no sibling to the right, so it itself is
+			// the last sibling to the right.
+			lastSibling = target;
+
+		lastSibling._siblingRight = this;
+		this._siblingLeft = lastSibling;
+
+		// Update all windows in the chain
+		var head = getLastLeftSibling();
+
+		if (head != null)
+			head.updateBasedOnSibling(head, true);
+	}
+
+	public function detach():Void
+	{
+		// TODO: implement this
+	}
+
+	public function getLastRightSibling():Window
+	{
+		var sibling:Window = _siblingRight;
+		while (sibling != null)
+		{
+			if (sibling._siblingRight == null)
+				break;
+			sibling = sibling._siblingRight;
+		}
+
+		return sibling;
+	}
+
+	public function getLastLeftSibling():Window
+	{
+		var sibling:Window = _siblingLeft;
+		while (sibling != null)
+		{
+			if (sibling._siblingLeft == null)
+				break;
+			sibling = sibling._siblingLeft;
+		}
+
+		return sibling;
 	}
 }
