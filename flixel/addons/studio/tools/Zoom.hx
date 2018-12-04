5712dcc88395d1dbf155932534fb00a54907092e
Fernando Bevilacqua <dovyski@gmail.com>
2018-12-04 11:59:34 +0100

    Re-factor and improve UX of Zoom tool

--- flixel/addons/studio/tools/Zoom.hx
+++ flixel/addons/studio/tools/Zoom.hx
@@ -18,8 +18,10 @@ import flixel.addons.studio.utils.Formtatter;
 class Zoom extends Tool
 {		
 	public static inline var ZOOM_CHANGE_STEP:Float = 0.02;
+	public static inline var TOOLTIP_TTL_SECONDS:Float = 1;
 	
-	var _tooltip:TooltipOverlay;	
+	var _tooltip:TooltipOverlay;
+	var _tooltipTTL:Float;
 	
 	override public function init(brain:Interaction):Tool 
 	{
@@ -36,35 +38,43 @@ class Zoom extends Tool
 	override public function update():Void 
 	{
 		super.update();
-		
+		updateTooltipOverlay();
+
 		var activationPressed = _brain.keyPressed(Keyboard.CONTROL);
 		var zoomIn = activationPressed && _brain.keyPressed(Keyboard.PERIOD);
 		var zoomOut = activationPressed && _brain.keyPressed(Keyboard.COMMA);
 		var change:Float = 0;
 
 		if (!zoomIn && !zoomOut)
-		{
-			if (_tooltip.visible)
-				_tooltip.setVisible(false);
 			return;
-		}
 
-		change = zoomIn ? ZOOM_CHANGE_STEP : -ZOOM_CHANGE_STEP;
+		change = ZOOM_CHANGE_STEP * (zoomIn ?  1 : -1);
 		FlxG.camera.setScale(FlxG.camera.scaleX + change, FlxG.camera.scaleY + change);
 
-		showOverlayTooltip();
+		showTooltipOverlay();
 	}
 
-	function showOverlayTooltip():Void
+	function updateTooltipOverlay():Void
+	{
+		if (_tooltipTTL > 0)
+			_tooltipTTL -= FlxG.elapsed;
+
+		if (_tooltipTTL <= 0 && _tooltip.visible)
+			_tooltip.setVisible(false);
+
+		_tooltip.setText(
+			"camera.scaleX: " + Formatter.prettifyFloat(FlxG.camera.scaleX) + "\n" +
+			"camera.scaleY: " + Formatter.prettifyFloat(FlxG.camera.scaleY)
+		);
+	}
+
+	function showTooltipOverlay():Void
 	{
 		if (!_tooltip.visible)
 			_tooltip.setVisible(true);
-
-		_tooltip.x = FlxG.camera.scroll.x;
-		_tooltip.y = FlxG.camera.scroll.y - FlxG.height * 0.7;
-		_tooltip.setText(
-			"scaleX: " + Formatter.prettifyFloat(FlxG.camera.scaleX) + "\n" +
-			"scaleY: " + Formatter.prettifyFloat(FlxG.camera.scaleY)
-		);
+			
+		_tooltip.x = FlxG.width * 0.4;
+		_tooltip.y = FlxG.height * 0.4;
+		_tooltipTTL = TOOLTIP_TTL_SECONDS;
 	}
 }
